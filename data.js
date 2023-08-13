bc0a50417817aefc5da26c0ef08babba2d00f451
Helio <heliodiaz91@gmail.com>
2023-08-13 19:52:14 +0200

    Portfolio Inicial

---
+++ data.js
@@ -0,0 +1,82 @@
+const proyectos = [
+    {
+        titulo: "Página Web Simple",
+        descripción: "Este proyecto lo hice para aprender distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"pagina-web-simple.png",
+        tecnologías: ["HTML", "CSS"]
+    },
+    {
+        titulo: "Mi segundo proyecto",
+        descripción: "Este proyecto lo hice para seguir aprendiendo distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["Angular", "TS"]
+    },
+    {
+        titulo: "Mi primer proyecto",
+        descripción: "Este proyecto lo hice para aprender distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["HTML", "CSS"]
+    },
+    {
+        titulo: "Mi segundo proyecto",
+        descripción: "Este proyecto lo hice para seguir aprendiendo distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["Angular", "TS"]
+    },
+    {
+        titulo: "Mi primer proyecto",
+        descripción: "Este proyecto lo hice para aprender distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["HTML", "CSS"]
+    },
+    {
+        titulo: "Mi segundo proyecto",
+        descripción: "Este proyecto lo hice para seguir aprendiendo distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["Angular", "TS"]
+    },
+    {
+        titulo: "Mi primer proyecto",
+        descripción: "Este proyecto lo hice para aprender distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["HTML", "CSS"]
+    },
+    {
+        titulo: "Mi segundo proyecto",
+        descripción: "Este proyecto lo hice para seguir aprendiendo distintas tecnologías web",
+        link: "https://www.google.com",
+        imagen:"",
+        tecnologías: ["Angular", "TS"]
+    },
+    
+]
+
+const informacionPersonal = {
+    imagen: "/img/FotoPerfilSinFondo.png",
+    otros:[
+        ["Nacionalidad", "Español"],
+        [
+            "Edad",
+            new Date(new Date() - new Date("1991/06/02")).getFullYear() - 1970
+        ],
+    ],
+    idiomas: [
+        ["Español", "Nativo"],["Ingles", "Medi-Alto"]
+    ],
+    tecnologías:[
+        ["Html", 7],["CSS", 7],["JavaScript",6],["TypeScript", 7],["Angular", 7],
+        ["C#", 6], ["Java", 8],["Spring", 5]
+    ],
+    redes:[
+        ["github", "https://github.com/HelioDC"],
+        ["linkedin", "https://www.linkedin.com/in/helio-díaz-campano-b58530240/"]
+    ]
+
+}
---
+++ index.js
@@ -0,0 +1,94 @@
+const proyectosContainer = document.getElementById("tarjetasContainer");
+const aside = document.getElementsByTagName("aside")[0];
+
+const getTecnologias = (tecnologias) => {
+    let res = "";
+    tecnologias.forEach(tecnologia => {
+        res += `<span class="tecnologia">${tecnologia}</span>`
+    })
+    return res;
+}
+
+// Creación proyectos
+proyectos.forEach(proyecto => {
+    const nuevoProyecto = document.createElement("div");
+    nuevoProyecto.classList= "tarjeta proyecto"; 
+    proyectosContainer.appendChild(nuevoProyecto);
+    nuevoProyecto.innerHTML = `
+    <img src="img/proyectos/${proyecto.imagen}">
+    <div>
+      <h3>${proyecto.titulo}</h3>
+      <p>${proyecto.descripción}</p>
+      <p>Tecnologías: ${getTecnologias(proyecto.tecnologías)} </p>
+    </div>
+    <a href="${proyecto.link}" target="_blank">Ver proyecto</a>
+    `
+})
+
+// Creación Info personal
+const nuevaPresentacion = document.createElement("div");
+nuevaPresentacion.classList = "presentacion";
+nuevaPresentacion.innerHTML = `
+    <img src="${informacionPersonal.imagen}" id="foto-personal">
+`;
+informacionPersonal.otros.forEach(dato => {
+    nuevaPresentacion.innerHTML += `
+      <div>
+        <span>${dato[0]}:</span>
+        <span>${dato[1]}</span>
+      </div>
+    `
+})
+aside.appendChild(nuevaPresentacion);
+// Idiomas
+const nuevoIdioma = document.createElement("div");
+nuevoIdioma.classList = "idioma";
+informacionPersonal.idiomas.forEach(dato => {
+    nuevoIdioma.innerHTML += `
+      <div>
+        <span>${dato[0]}:</span>
+        <span>${dato[1]}</span>
+      </div>
+    `
+})
+aside.appendChild(nuevoIdioma);
+// Lenguajes de programación
+const nuevoLenguajesProgramacion = document.createElement("div");
+nuevoLenguajesProgramacion.classList = "tecnologías";
+informacionPersonal.tecnologías.forEach(dato => {
+    nuevoLenguajesProgramacion.innerHTML += `
+      <div>
+      <span>${dato[0]}</span>
+        <span>${dato[1]}</span>
+        </div>
+      <progress max="10" value=${dato[1]}>
+    `
+})
+aside.appendChild(nuevoLenguajesProgramacion);
+
+//Links
+function getIconoRed(red){
+    switch(red){
+        case "linkedin":
+            return "linkedin.svg";
+            break;
+        case "github":
+            return "github.svg";
+            break;
+        default:
+            return "globe-solid.svg";
+    }
+}
+
+const nuevoRedes = document.createElement("div");
+nuevoRedes.classList = "redes";
+informacionPersonal.redes.forEach(dato => {
+    if(dato[1] === "") return; 
+    nuevoRedes.innerHTML += `
+      <a href="${dato[1]}" target="_blank">
+        <img src="img/iconos/${getIconoRed(dato[0])}" alt="foto">
+      </a>
+    `
+})
+aside.appendChild(nuevoRedes);
+
